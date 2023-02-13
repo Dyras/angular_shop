34ae4e867a7b560822adce8fcee8abd66b90d921
Johan Forsberg <jevwmguf@duck.com>
2023-02-13 15:38:12 +0100

    Adds most of the modules and components and adds the routing

---
+++ src/app/product-page/product-page.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ProductPageComponent } from './product-page.component';
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [ProductPageComponent],
+  declarations: [ProductPageComponent],
+  providers: [],
+})
+export class ProductPageModule {}
