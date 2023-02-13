a2487e7e622a585948e21b5900cb295175a0c3a0
Johan Forsberg <jevwmguf@duck.com>
2023-02-13 15:58:36 +0100

    Rename IProduct to product and start working on the service

---
+++ src/app/products/products.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn : 'root'
+})
+
+export class ProductService(){
+
+    // Create getProducts() method
+    getProducts(): Product[] {
+    
+}
+}
