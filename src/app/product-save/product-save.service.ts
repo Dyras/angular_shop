fac224b3ddfef2f7e417cfc74dc26b527cd1c4a9
Johan Forsberg <jevwmguf@duck.com>
2023-02-16 10:50:44 +0100

    Create a service to handle the product keeping in localStorage

---
+++ src/app/product-save/product-save.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductSaveService {
+  constructor() {}
+
+  localStorageWriter(id: number, amount: number) {
+    const storageCheck = localStorage.getItem(id.toString());
+    if (storageCheck === null) {
+      localStorage.setItem(id.toString(), '1');
+    } else if (storageCheck !== null && amount > 0) {
+      const currentCount = localStorage.getItem(id.toString());
+      const newCount = parseInt(currentCount) + 1;
+      localStorage.setItem(id.toString(), newCount.toString());
+      console.log(e);
+    } else if (storageCheck !== null && amount < 0) {
+      const currentCount = localStorage.getItem(id.toString());
+      const newCount = parseInt(currentCount) - 1;
+      localStorage.setItem(id.toString(), newCount.toString());
+    }
+  }
+
+  localStorageChecker(id: number) {
+    const storageCheck = localStorage.getItem(id.toString());
+    if (storageCheck === null) {
+      return 0;
+    } else {
+      return parseInt(storageCheck);
+    }
+  }
+}
