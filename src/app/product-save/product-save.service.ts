9973e40460f2568c28f2a1c4d97b83d05aa1200c
Johan Forsberg <jevwmguf@duck.com>
2023-02-20 14:42:16 +0100

    The default object can no longer be added to the cart

--- src/app/product-save/product-save.service.ts
+++ src/app/product-save/product-save.service.ts
@@ -8,32 +8,34 @@ export class ProductSaveService {
   constructor() {}
 
   addToCart(product: IProduct, amount: number) {
-    let storageArray: IProductSaved[];
-    storageArray = JSON.parse(localStorage.getItem('cart') || '[]');
-    let idPlacement = null;
-    for (let i = 0; i < storageArray.length; i++) {
-      if (storageArray[i].id === product.id) {
-        idPlacement = i;
+    if (product.id !== '0') {
+      let storageArray: IProductSaved[];
+      storageArray = JSON.parse(localStorage.getItem('cart') || '[]');
+      let idPlacement = null;
+      for (let i = 0; i < storageArray.length; i++) {
+        if (storageArray[i].id === product.id) {
+          idPlacement = i;
+        }
       }
-    }
 
-    if (idPlacement == null) {
-      storageArray.push({ ...product, amount: amount + 1 });
-      console.log(storageArray);
-      localStorage.setItem('cart', JSON.stringify(storageArray));
-    } else if (idPlacement !== null && amount == 0) {
-      storageArray[idPlacement].amount = storageArray[idPlacement].amount + 1;
-      localStorage.setItem('cart', JSON.stringify(storageArray));
-    } else if (idPlacement !== null && amount >= 0) {
-      storageArray[idPlacement].amount = storageArray[idPlacement].amount + 1;
-      localStorage.setItem('cart', JSON.stringify(storageArray));
-    } else if (idPlacement !== null && amount < 0) {
-      storageArray[idPlacement].amount = storageArray[idPlacement].amount - 1;
-
-      if (storageArray[idPlacement].amount == 0) {
-        storageArray.splice(idPlacement, 1);
+      if (idPlacement == null) {
+        storageArray.push({ ...product, amount: amount + 1 });
+        console.log(storageArray);
         localStorage.setItem('cart', JSON.stringify(storageArray));
-      } else localStorage.setItem('cart', JSON.stringify(storageArray));
+      } else if (idPlacement !== null && amount == 0) {
+        storageArray[idPlacement].amount = storageArray[idPlacement].amount + 1;
+        localStorage.setItem('cart', JSON.stringify(storageArray));
+      } else if (idPlacement !== null && amount >= 0) {
+        storageArray[idPlacement].amount = storageArray[idPlacement].amount + 1;
+        localStorage.setItem('cart', JSON.stringify(storageArray));
+      } else if (idPlacement !== null && amount < 0) {
+        storageArray[idPlacement].amount = storageArray[idPlacement].amount - 1;
+
+        if (storageArray[idPlacement].amount == 0) {
+          storageArray.splice(idPlacement, 1);
+          localStorage.setItem('cart', JSON.stringify(storageArray));
+        } else localStorage.setItem('cart', JSON.stringify(storageArray));
+      }
     }
   }
 
