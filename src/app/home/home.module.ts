d56b65b627d6c9e95c0673c3379268297d8c70dc
Johan Forsberg <jevwmguf@duck.com>
2023-02-23 14:15:13 +0100

    Add queryParams and routing to the front page links

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [HomeComponent],
   declarations: [HomeComponent],
   providers: [],
--- src/app/products-page/products-page.component.ts
+++ src/app/products-page/products-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 import { IProduct } from '../products/product';
 import { ProductService } from '../products/products.service';
@@ -10,15 +11,31 @@ import { ProductService } from '../products/products.service';
 export class ProductsPageComponent {
   products$: BehaviorSubject<IProduct[]> = new BehaviorSubject<IProduct[]>([]);
 
-  constructor(private productService: ProductService) {
-    this.fetchProducts();
+  constructor(
+    private productService: ProductService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ) {
+    let whichWine;
+    this._route.queryParams.subscribe((params) => {
+      if (params['type']) {
+        whichWine = params['type'];
+      }
+    });
+    console.log(whichWine);
+    if (whichWine != undefined) {
+      this.fetchProducts(whichWine);
+    } else {
+      this.fetchProducts('all');
+    }
   }
 
-  async fetchProducts() {
-    this.products$.next(await this.productService.getProducts());
-  }
-
-  async filterProducts(filter: string) {
+  async fetchProducts(filter: string) {
     this.products$.next(await this.productService.getProductsByType(filter));
+    console.log(filter);
+    this._router.navigate(['/products'], {
+      queryParamsHandling: 'merge',
+      queryParams: { type: filter },
+    });
   }
 }
