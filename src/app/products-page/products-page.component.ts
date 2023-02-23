def8aed5506628dcd1ab0122bcb36032acc846c3
Johan Forsberg <jevwmguf@duck.com>
2023-02-23 15:46:29 +0100

    Fixes a bug where the Firestore service would run twice

--- src/app/products-page/products-page.component.ts
+++ src/app/products-page/products-page.component.ts
@@ -10,7 +10,7 @@ import { ProductService } from '../products/products.service';
 })
 export class ProductsPageComponent {
   products$: BehaviorSubject<IProduct[]> = new BehaviorSubject<IProduct[]>([]);
-
+  lastChosenFilter: string | null = null;
   constructor(
     private productService: ProductService,
     private _router: Router,
@@ -20,22 +20,25 @@ export class ProductsPageComponent {
     this._route.queryParams.subscribe((params) => {
       if (params['type']) {
         whichWine = params['type'];
+        this.fetchProducts(whichWine);
+      } else {
+        whichWine = 'all';
+        this._router.navigate(['/products'], {
+          queryParamsHandling: 'merge',
+          queryParams: { type: 'all' },
+        });
       }
     });
-    console.log(whichWine);
-    if (whichWine != undefined) {
-      this.fetchProducts(whichWine);
-    } else {
-      this.fetchProducts('all');
-    }
   }
 
   async fetchProducts(filter: string) {
-    this.products$.next(await this.productService.getProductsByType(filter));
-    console.log(filter);
-    this._router.navigate(['/products'], {
-      queryParamsHandling: 'merge',
-      queryParams: { type: filter },
-    });
+    if (filter != this.lastChosenFilter) {
+      this._router.navigate(['/products'], {
+        queryParamsHandling: 'merge',
+        queryParams: { type: filter },
+      });
+      this.lastChosenFilter = filter;
+      this.products$.next(await this.productService.getProductsByType(filter));
+    }
   }
 }
