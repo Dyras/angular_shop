f3a71fb2783b649420474fbcdf867363548b8e46
Johan Forsberg <jevwmguf@duck.com>
2023-02-27 15:51:08 +0100

    Replace buttons with mini-fab. Keep working on the checkout CSS.

--- src/app/checkout/checkout.module.ts
+++ src/app/checkout/checkout.module.ts
@@ -2,9 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from '../app-routing.module';
 import { CheckoutComponent } from './checkout.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [CommonModule, AppRoutingModule],
+  imports: [CommonModule, AppRoutingModule, MatIconModule, MatButtonModule],
   exports: [CheckoutComponent],
   declarations: [CheckoutComponent],
 })
