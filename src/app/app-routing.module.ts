edbc618325d6895f1032149f0f38079953c97969
Johan Forsberg <12563436+Dyras@users.noreply.github.com>
2023-03-01 13:31:25 +0100

    Adds the privacy page and adds it to the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { HomeComponent } from './home/home.component';
+import { PrivacyPageComponent } from './privacy-page/privacy-page.component';
 import { ProductsPageComponent } from './products-page/products-page.component';
 import { SingleProductComponent } from './singleproduct/singleproduct.component';
 
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: 'checkout',
     component: CheckoutComponent,
+  },
+  {
+    path: 'privacy',
+    component: PrivacyPageComponent,
   },
   {
     path: '**',
---
+++ src/app/privacy-page/privacy-page.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { PrivacyPageComponent } from './privacy-page.component';
+
+@NgModule({
+  imports: [],
+  exports: [PrivacyPageComponent],
+  declarations: [PrivacyPageComponent],
+  providers: [],
+})
+export class PrivacyPageModule {}
