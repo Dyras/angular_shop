987ef4d90ca7d82725c90d86ebf558dc068068a8
Johan Forsberg <jevwmguf@duck.com>
2023-03-06 14:08:10 +0100

    Moves log in/out and register into a dropdown menu

--- src/app/navbar/navbar.module.ts
+++ src/app/navbar/navbar.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar.component';
 import { MatBadgeModule } from '@angular/material/badge';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, MatBadgeModule],
+  imports: [CommonModule, RouterModule, MatBadgeModule, MatSelectModule],
   exports: [NavbarComponent],
   declarations: [NavbarComponent],
 })
